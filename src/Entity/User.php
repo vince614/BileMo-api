cd1d6062d9b8310b3f77544f5fcf225e8121b6f6
vince <vaufreyvincent@gmail.com>
2022-10-07 16:31:10 +0200

    Configuration API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,21 +3,48 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\Api\ApiController;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource]
+#[ApiResource(
+    collectionOperations: [
+        'showAll' => [
+            'method' => 'GET',
+            'route_name' => 'api_users_client_showAll',
+            'controller' => ApiController::class,
+        ],
+        'post'
+    ],
+    itemOperations: [
+        'get' => [
+            'method' => 'GET',
+            'route_name' => 'api_user_client',
+            'controller' => ApiController::class,
+            'normalization_context' => [
+                'groups' => 'user:item'
+            ]
+        ],
+        'delete'
+    ],
+    shortName: 'Utilisateurs',
+    order: ['username' => 'DESC'],
+    paginationItemsPerPage: 10,
+)]
 class User implements PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user:list', 'user:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['user:list', 'user:item'])]
     private ?string $email = null;
 
     /**
@@ -27,10 +54,12 @@ class User implements PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user:list', 'user:item'])]
     private ?string $username = null;
 
     #[ORM\ManyToOne(inversedBy: 'users')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['user:list', 'user:item'])]
     private ?Client $client = null;
 
     public function getId(): ?int
